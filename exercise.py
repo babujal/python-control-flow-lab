d089b6bad601fcd4f9cacd1add679f0a36ae1d4f
babujal <dercamar1@gmail.com>
2024-11-14 11:24:42 -0500

    another way to hadle error message

--- exercise.py
+++ exercise.py
@@ -80,8 +80,34 @@ def check_voting_eligibility():
             print('You are not eligible to vote')
         else:
             print('You are eligible to vote')
-    except ValueError:
-        print('Wrong age value, please enter a nummber for age value.')
+    except ValueError as e:
+        print(e)
 # Call the function
 check_voting_eligibility()
+
+#///////////////////////////////////////////////////////////////////////////
+
+# Exercise 3: Calculate Dog Years
+#
+# Write a Python function named `calculate_dog_years` that calculates a dog's age in dog years.
+# Fill in the logic to perform the calculation inside the function.
+#
+# Function Details:
+# - Prompt the user to enter a dog's age: "Input a dog's age: "
+# - Calculate the dog's age in dog years:
+#      - The first two years of the dog's life count as 10 dog years each.
+#      - Each subsequent year counts as 7 dog years.
+# - Print the calculated age: "The dog's age in dog years is xx."
+# - Replace 'xx' with the calculated dog years.
+#
+# Hints:
+# - Use the `input()` function to capture user input.
+# - Convert the string input to an integer using `int()`.
+# - Apply conditional logic to perform the correct age calculation based on the dog's age.
+
+# def calculate_dog_years():
+    # Your control flow logic goes here
+
+# Call the function
+# calculate_dog_years()
 
